e37857ab183f293f32eb5b426669a1df2286c933
MrunalDesale <mrunalsd94@gmail.com>
2016-03-01 15:02:39 +0530

    Rest call demo

---
+++ app/src/main/java/shoppingpad/com/restcalldemo/Person.java
@@ -0,0 +1,35 @@
+package shoppingpad.com.restcalldemo;
+
+/**
+ * Created by bridgelabz3 on 29/2/16.
+ */
+public class Person
+{
+    private String name;
+    private String country;
+    private String twitter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+}
